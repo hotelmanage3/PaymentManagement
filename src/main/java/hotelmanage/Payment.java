52d94b596b4fc79bb0f0d56957e77b4d52329c3f
jst267 <64456259+jst267@users.noreply.github.com>
2020-05-13 16:14:21 +0900

    Update Payment.java

--- src/main/java/hotelmanage/Payment.java
+++ src/main/java/hotelmanage/Payment.java
@@ -19,7 +19,7 @@ public class Payment {
     private String ReservationStatus; //받아야되고
     private String PaymentStatus;//결재될떄 "Y" 로 셋팅
 
-    @PrePersist
+    @PostPersist
     public void onPrePersist() {
 
         if ("reserve".equals(ReservationStatus) ) {
